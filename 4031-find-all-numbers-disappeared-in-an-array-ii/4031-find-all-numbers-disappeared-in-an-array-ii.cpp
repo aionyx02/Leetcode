c6568370d30e09101294ed09615463018af12799
Aionyx <aionyxhuang@gmail.com>
2026-08-26 14:54:04 +0800

    Time: 168 ms (32.02%), Space: 276.9 MB (33.25%) - LeetHub

---
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
+        unordered_set<int> present(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        int start = -1;
+
+        for (int i = lower; i <= upper; ++i) {
+            if (present.find(i) == present.end()) {
+                if (start == -1)
+                    start = i;
+            }
+            else {
+                if (start != -1) {
+                    ans.push_back({start, i - 1});
+                    start = -1;
+                }
+            }
+        }
+        if (start != -1) {
+            ans.push_back({start, upper});
+        }
+
+        return ans;
+    }
+};
