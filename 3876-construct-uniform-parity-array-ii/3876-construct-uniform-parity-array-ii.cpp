759d19784c563129197c8de3bda69c667ef1c653
Aionyx <aionyxhuang@gmail.com>
2026-09-03 10:57:04 +0800

    Time: 8 ms (47.38%), Space: 165.7 MB (99.48%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int odd = 0, even = 0;
+        int odd = 0;
         int min_odd = INT_MAX, min_even = INT_MAX;
         for (int x : nums1) {
             if (x % 2 == 0) {
-                even += 1;
                 min_even = min(min_even, x);
             }
             else {
@@ -14,6 +13,6 @@ public:
             }
         }
 
-        return odd + even == 1 || min_odd < min_even || odd == 0; 
+        return nums1.size() == 1 || min_odd < min_even || odd == 0; 
     }
 };
