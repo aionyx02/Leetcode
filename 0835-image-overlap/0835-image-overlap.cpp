348954f765c4ffe9a44810ce433b961ead35fd7b
Aionyx <aionyxhuang@gmail.com>
2026-09-13 10:19:10 +0800

    Time: 47 ms (71.08%), Space: 12.9 MB (68.83%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int ans = 0;
+
+        for (int dx = -n + 1; dx < n; ++dx) {
+            for (int dy = -n + 1; dy < n; ++dy) {
+                int current_overlap = 0;
+                
+                for (int i = 0; i < n; ++i) {
+                    for (int j = 0; j < n; ++j) {
+                        int r = i + dx;
+                        int c = j + dy;
+                        
+                        if (r >= 0 && r < n && c >= 0 && c < n) {
+                            current_overlap += img1[i][j] * img2[r][c];
+                        }
+                    }
+                }
+                ans = std::max(ans, current_overlap);
+            }
+        }
+        return ans;
+    }
+};
